9b4325b32f507d73a472e00a66ba55d72400d599
MuteeVincente <vinniemutee@gmail.com>
2023-09-03 09:50:47 +0300

    transaction component key

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -8,7 +8,7 @@ const TransactionList = () => {
     <>
     <h3>History</h3>
     <ul id='list' className='list'>
-      {transactions.map(transaction => (<Transaction transaction={transaction}/>))}
+      {transactions.map(transaction => (<Transaction key = {transaction.id} transaction={transaction}/>))}
         
 
     </ul>
