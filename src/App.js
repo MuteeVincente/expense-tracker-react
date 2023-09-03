e9aa9ae38f4aee658d14e477fe7a906af843d95c
MuteeVincente <vinniemutee@gmail.com>
2023-09-03 07:18:37 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Header/>
     </div>
   );
 }
