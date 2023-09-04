9862745e7d475213171699fd6c0c2ab7f02244e5
MuteeVincente <vinniemutee@gmail.com>
2023-09-04 08:22:25 +0300

    math.random to generate id

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,27 @@
-import React, {useState} from 'react'
+import React, {useState,useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState';
+
 
 const AddTransactionList = () => {
   const[text,setText] = useState('');
   const[amount,setAmount] = useState(0);
 
+  const {addTransaction} = useContext(GlobalContext);
+
+  const onSubmit = e =>{
+    e.preventDefault();
+
+
+    //The Math.random() static method returns a floating-point, pseudo-random number that's greater than or equal to 0 and less than 1,
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000)
+    }
+  }
+
   return (
     <>
     <h3>Add new transaction</h3>
-    <form>
+    <form  onSubmit={onSubmit}>
         <div className='form-control'>
             <label htmlFor ='text'>Text</label>
             <input type='text' value={text} 
